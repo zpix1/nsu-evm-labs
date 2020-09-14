ba5ba1d52f4aa25c42f18e47dcc78a2eaeaa0141
Ivan Baksheev <zpix-dev@list.ru>
2020-09-14 19:08:17 +0700

    lab2: plot added

--- 2/plot.py
+++ 2/plot.py
@@ -1,19 +1,25 @@
+#%% 
+#!/usr/bin/python3.6
 from json import load
 import matplotlib
 import matplotlib.pyplot as plt
 
+if __name__ == "__main__":
+    flags = ['-O0', '-O1', '-O2', '-O3', '-Os', '-Ofast', '-Og']
 
-flags = ['-O0', '-O1', '-O2', '-O3', '-Os', '-Ofast', '-Og']
-data = load(open('out.json', 'r'))
-assert(len(data) == len(flags))
+    data = load(open('out.json', 'r'))
 
-for i in range(len(data)):
-    plt.plot(data[i][0], data[i][1])
+    assert(len(data) == len(flags))
 
-plt.title('Зависимость времени исполнения от различных флагов компиляции')
-plt.legend(flags, loc='upper left')
-plt.xlabel('Взятая часть N')
-plt.ylabel('Время исполнения, сек.')
-plt.xticks(data[0][0] , map(lambda x: f'{x}', data[0][0]))
-plt.show()
-plt.savefig('out.png')
+    for i in range(len(data)):
+        plt.plot(data[i][0], data[i][1])
+
+    plt.title('Зависимость времени исполнения от различных флагов компиляции')
+    plt.legend(flags, loc='upper left')
+    plt.xlabel('Взятая часть N')
+    plt.ylabel('Время исполнения, сек.')
+    plt.xticks(data[0][0] , map(lambda x: f'{x}', data[0][0]))
+    # plt.show()
+    plt.savefig('out.png')
+
+# %%
--- 2/test.py
+++ 2/test.py
@@ -5,7 +5,7 @@ from os.path import getsize
 import time
 
 def get_time(flag, n):
-    c_command = split(f'/usr/local/Cellar/gcc/9.2.0_1/bin/gcc-9 {flag} main.c -o main.out')
+    c_command = split(f'gcc {flag} main.c -o main.out')
     start_time = time.time()
     check_output(c_command)
     end_time = time.time()
@@ -15,27 +15,25 @@ def get_time(flag, n):
     return {'ctime': end_time - start_time, 
             'rtime': out['time'], 
             'size': getsize('main.out'), 
-            'md5': check_output(split('md5 main.out')).decode().strip()}
+            'md5': check_output(split('md5sum main.out')).decode().strip()}
 
-# p = plt.plot()
+if __name__ == "__main__":
+    n = 5000000000
+    data = []
 
-n = 50000000
-data = []
-
-for flag in ['-O0', '-O1', '-O2', '-O3', '-Os', '-Ofast', '-Og']:
-    xv = []
-    yv = []
-    cv = []
-    for k in [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75]:
-        print(k, flag)
-        t = get_time(flag, n * k)
-        xv.append(k)
-        yv.append(t['rtime'])
-        cv.append(t['ctime'])
-        print(t)
-    data.append([xv, yv, cv])
-    # break
-print(data)
-with open('out.json', 'w') as f:
-    f.write(dumps(data))
-# plt.show()
+    for flag in ['-O0', '-O1', '-O2', '-O3', '-Os', '-Ofast', '-Og']:
+        xv = []
+        yv = []
+        cv = []
+        for k in [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75]:
+            print(k, flag)
+            t = get_time(flag, n * k)
+            xv.append(k)
+            yv.append(t['rtime'])
+            cv.append(t['ctime'])
+            print(t)
+        data.append([xv, yv, cv])
+    
+    print(data)
+    with open('out.json', 'w') as f:
+        f.write(dumps(data))
